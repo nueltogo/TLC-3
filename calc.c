964fe004af19acc3b2bb85ec13d996444f7cec5e
nueltogo <nunana.togo@turntabl.io>
2021-01-25 14:50:10 +0000

    mode for calc.c

--- calc.c
+++ calc.c
@@ -1,15 +1,21 @@
 #include <iostream>
 int var1, var2, var3, var4, var5;
-
-int mean_value(var1, var2, var3, var4, var5)
+int array[] = {var1,var2,var3,var4,var5};
+int mean_value(int array[])
 {
-	int mean = (var1+var2+var3+var4+var5)
+	int mean = (array[0]+array[1]+array[2]+array[3]+array[4])/5
 	return mean;
 }
 
-int mode_value(var1,var2,var3,var4,var5)
+int mode_value(int array[])
 {
-	
+	int mode = 0;
+	int count_array[] = {};
+	int sorted_array = sort(array, array + 1);
+	for(int i, i < 5, i++)
+	{
+
+	}
 }
 
 int main()
