a0f0860517b1f62307477be694ee75de34809fd4
nueltogo <nunana.togo@turntabl.io>
2021-01-25 15:16:23 +0000

    mode addition to calc.c

--- calc.c
+++ calc.c
@@ -1,27 +1,27 @@
 #include <iostream>
 int var1, var2, var3, var4, var5;
-int array[] = {var1,var2,var3,var4,var5};
-int mean_value(int array[])
+float mean_value(int array[])
 {
-	int mean = (array[0]+array[1]+array[2]+array[3]+array[4])/5
+	float mean = (array[0]+array[1]+array[2]+array[3]+array[4])/5;
 	return mean;
 }
-
+/*
 int mode_value(int array[])
 {
 	int mode = 0;
 	int count_array[] = {};
-	int sorted_array = sort(array, array + 1);
+	int sorted_array = qsort(array, array + 5);
 	for(int i, i < 5, i++)
 	{
 
 	}
 }
-
+*/
 int main()
 {
 	std::cout << "Please Enter Five Numbers" << std::endl;
 	std::cin >> var1 >> var2 >> var3 >> var4 >> var5;
-	int mean = mean_value(var1,var2,var3,var4,var5);
+	int array[] = {var1,var2,var3,var4,var5};
+	float mean = mean_value(array);
 	std::cout << "The meand of the values is: " << mean << std::endl;
 }
